14f0d4e4b7e42927204023ed126fa12db862848e
Murphy <murphytian06@gmail.com>
2023-04-29 23:30:42 -0400

    add AddTwoNumbers.py

---
+++ AddTwoNumbers.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# You are given two non-empty linked lists representing two non-negative
+# integers. The digits are stored in reverse order, and each of their nodes
+# contains a single digit. Add the two numbers and return the sum as a linked
+# list.
+# You may assume the two numbers do not contain any leading zero, except the
+# number 0 itself.
+class Solution:
+    def add_two_numbers(self, l1: Optional[ListNode],
+                      l2: Optional[ListNode]) -> Optional[ListNode]:
+        
