3e8390d55af81612fd1e8a4ef513b7bbd103a57d
Murphy <murphytian06@gmail.com>
2024-07-12 12:24:17 -0400

    Create IntersectionOfTwoLinkedList.py

---
+++ LinkedList/IntersectionOfTwoLinkedList.py
@@ -0,0 +1,17 @@
+# Leetcode link: https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+
+        A, B = headA, headB
+        while A != B:
+            A = A.next if A else headB
+            B = B.next if B else headA
+        
+        return A
